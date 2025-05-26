a84997a86b61271ba74152f83b213a4d3f45dccc
AgusC02 <164360053+AgusC02@users.noreply.github.com>
2025-05-25 23:49:20 -0300

    Revert "aaaaaaaaaaaaaa"
    This reverts commit b639817a7c342c92fa41bc671ac1ef567a95e65d.

--- MV.h
+++ MV.h
@@ -4,20 +4,19 @@
 #define CANTFUNC 31
 #define CANTREG 16
 #define CANTERRORES 3
-#define CANTMAXSEGMENTOS 8
 #define TDDSSIZE 8
 //DEFINES PARA EL SYS
 #define BITS_32 32
 #define BUF_SIZE (BITS_32 + 2)
+
 // Define de registros
 #define CS 0
 #define DS 1
-#define ES 2
-#define SS 3
-#define KS 4
+//#define ES 2
+//#define SS 3
 #define IP 5
-#define SP 6
-#define BP 7
+//#define SP 6
+//#define BP 7
 #define CC 8
 #define AC 9
 #define EAX 10
@@ -27,36 +26,18 @@
 #define EEX 14
 #define EFX 15
 
-#define ERRDIV0 0
-#define ERRINVINST 1
-#define ERRSEGMF 2
-#define ERRMEM 3
-#define ERRSTOVF 4
-#define ERRSTUNF 5
 //Estan todas las funciones que solicitaba el enunciado del TP de MV pero no llegu� a corregir los errores de compilacion, estar�n corregidos para la siguiente entrega de la parte 1
 typedef struct theader{
     unsigned char c1,c2,c3,c4,c5;
     char version;
-    unsigned short int tamCS,tamDS,tamES,tamSS,tamKS,entrypointoffset;
+    unsigned short int tam;
 }theader;
-
-typedef struct theadervmi{
-    unsigned char carV,carM,carI,car2,car5;
-    char version;
-    unsigned short int mem_size;
-}theadervmi;
 
 typedef struct TMV{
   unsigned char MEM[TMEM]; //Memoria, unsigned ya que la memoria no puede usar complemento a 2, nosotros tenemos que darle la interpretaci�n de si es negativo o positivo.
   int R[CANTREG]; //Registros
   int TDS[TDDSSIZE]; //Segmentos
-  int mem_size;
-  int size_paramsegment;
-  int argc;
-  int punteroargv;
-  char *archivovmi;
-  char disassembler;
-  char flagdebug;
+  int Errores[CANTERRORES];
 }TMV;
 
 typedef struct TInstruc{
@@ -67,23 +48,9 @@ typedef struct TInstruc{
 
 typedef void (*TFunc[CANTFUNC])(TMV *mv,TInstruc instruc); //Array de punteros a funciones
 
-void iniciasubrutina(TMV *MV);
-void mododebug(TMV *MV);
-void generarImagen(TMV MV);
-void init_mem0(TMV *MV);
-void init_reg0(TMV *MV);
-void init_tds0(TMV *MV);
-void inicializoMVen0(TMV *MV);
-void initparametrosMV(TMV *MV);
-void armaParamSegment(TMV *MV,int argc,char *argv[],int *paramsize);
-void dep_arg(int argc,char *argv[],TMV *MV);
-void initregsegmentos(TMV *MV);
-void agregasegmentos(unsigned short int tam, int reg_indx,TMV *MV, int *tds_indx, int sizeac);
-void inicializoTDS(TMV* MV,theader header);
-void inicializoRegistros(TMV *MV,theader header);
+void inicializoTDS(TMV* MV,short int TamCS);
+void inicializoRegistros(TMV *MV);
 void inicializoErrores(TMV *MV);
-void initheadervmx(theader *head);
-void agregoalconstantsegment(TMV *MV,int offset, unsigned char c_agregable);
 void generaerror(int tipo);
 void inicializoVecFunciones(char VecFunciones[CANTFUNC][5]); //PARA DISASSEMBLER
 void inicializoVecRegistros(char VecRegistros[CANTREG][4]);  //PARA DISASSEMBLER
@@ -106,10 +73,8 @@ int devuelveN(TMV *MV);
 int devuelveZ(TMV *MV);
 int leer_binario_c2_32(void);
 char *int_to_c2bin(int numero);
-void setvaloresSYS(TMV MV,char *mod, char *cantceldas, char *size, int *pos_i, int *pos_max);
+
 //DEBUG
-void muestraheader(theader h);
-void muestraMVfijos(TMV MV);
 void muestramemoria(unsigned char memoria[]);
 void muestraregistros(int reg[]);
 void muestratds(int tds[]);
@@ -169,15 +134,7 @@ void JNN(TMV * MV,TInstruc instruc);
 
 void NOT(TMV * MV,TInstruc instruc);
 
-void PUSH(TMV *MV, TInstruc instruc);
-
-void POP(TMV *MV, TInstruc instruc)
-
-void CALL(TMV *MV, TInstruc instruc);
-
 //0 OPERANDOS
-
-void RET(TMV * MV,TInstruc instruc);
 
 void STOP(TMV * MV,TInstruc instruc);
 
@@ -185,5 +142,5 @@ void STOP(TMV * MV,TInstruc instruc);
 void LeoInstruccionesDissasembler(TMV MV,char VecFunciones[CANTFUNC][5],char VecRegistros[CANTREG][4]);
 void EscriboDissasembler(TMV MV, char VecFunciones[CANTFUNC][5],char VecRegistros[CANTREG][4], unsigned char CodOp, TInstruc instruc,unsigned short int PosInicial,unsigned short int PosMemoria);
 void GuardoSector(char Segmento[4],unsigned char Sec);
-void clearscreen();
+
 #endif // MV_H_INCLUDED
--- main.c
+++ main.c
@@ -12,26 +12,20 @@ int main(int argc, char *argv[]){
     char nomarch[50];
 
     // INICIO
-    //strcpy(nomarch,argv[1]);
-    //LeoArch(nomarch,&MV);
+    strcpy(nomarch,argv[1]);
+    LeoArch(nomarch,&MV);
 
     inicializoVecFunciones(VecFunciones);
     inicializoVecRegistros(VecRegistros);
-
-    dep_arg(argc,argv,&MV);
-    /* LO DEJO COMENTADO PORQUE ASI FUNCIONABA EN LA PARTE 1 POR SI ACASO
     if(argc>2){
         if (!strcmp(argv[2],"-d")){
             LeoInstruccionesDissasembler(MV,VecFunciones,VecRegistros);
         }
     }
     LeoInstruccion(&MV);
-    */
 
-    LeoInstruccion(&MV);
     //PARA DEBUGEAR
     //muestravaloresmv(MV);
-    free(MV.archivovmi);
 
 return 0;
 }
